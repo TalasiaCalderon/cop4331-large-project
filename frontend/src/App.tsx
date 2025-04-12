4c2f3e9b16ae3730545ebdb5bac9a3405d150c5b
aneesh-vellanki <aneeshshanmukh123@gmail.com>
2025-04-12 19:49:01 -0400

    Update App.tsx to include the dashboard page
    I think we have to remove the home page since I just named the page after login as dashboard page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import HomePage from './pages/HomePage';
-import './App.css'
+import DashboardPage from './pages/DashboardPage';
+import './App.css';
 
 function App() {
-
   return (
-    <Router >
+    <Router>
       <Routes>
         <Route path="/" element={<SplashPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
